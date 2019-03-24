4b4a8aea63ab50d621d3f3d77c00efc6b0f7e547
Ganesh Ram <ganeshram997@gmail.com>
2019-03-24 15:52:43 -0400

    api ready

--- dockerApp/app.py
+++ dockerApp/app.py
@@ -2,19 +2,27 @@ from flask import Flask, render_template, request
 import sys
 import optparse
 import time
-from train import *
+import train
 
 app = Flask(__name__)
 start = int(round(time.time()))
 
-@app.route("/")
+@app.route("/internalDashboard")
 def send_index_file():
-	#train.hello()
+	train.hello()
 	return render_template("index.html")
 
-@app.route("getModelAPrediction")
+@app.route("/getModelAPrediction")
 def getModelAPrediction():
 	print("Model A prediction")
+	result = train.SmartEnergerA.predict("some date", 1, 2)
+	return result
+
+@app.route("/getModelBPrediction")
+def getModelBPrediction():
+	print("Model B prediction")
+	result = train.SmartEnergerB.predict("some date", 1,2,3)
+	return result
 
 
 if __name__ == '__main__':
